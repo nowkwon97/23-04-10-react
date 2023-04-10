f9000df9e130783ed5f912dd12247c5ecd7a22e5
leejaekwon <nowkwon97@gmail.com>
2023-04-10 16:53:26 +0900

    props 기본값 설정 및 연습

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,7 +1,8 @@
 import MyComponent from "./MyComponent";
 
 const App = () => {
-  return <MyComponent name = "React" />;
+  // props 에 값을 할당하지 않을 시 MyComponent.js에서 설정한 기본값이 들어간다.
+  return <MyComponent />;
 };
 
 export default App;
