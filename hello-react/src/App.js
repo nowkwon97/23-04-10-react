fe1abf02c035b4b3e2cbe6d7a79d0bd45c188ce8
leejaekwon <nowkwon97@gmail.com>
2023-04-10 15:05:42 +0900

    class가 아닌 className이다.

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,15 +1,8 @@
+import './App.css'
 
 function App() {
   const name = '리액트';
-  const style = {
-    // background-color는 backgroundColor와 같이 카멜 표기법으로 작성된다.
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: '48px', // font-size -> fontSize
-    fontWeight: 'bold', // font-weight -> fontWeight
-    padding: '16' // 단위를 생략하면 px로 지정된다.
-  };
-  return <div style={style}>{name} </div>;
+  return <div className = "react">{name}</div>
 }
 
 export default App;
