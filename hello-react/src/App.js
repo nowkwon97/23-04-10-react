8c7e131bdcde57fbe714834975546cdd4d3a8cf1
leejaekwon <nowkwon97@gmail.com>
2023-04-11 10:03:22 +0900

    propTypes 설정 및 isRequired 연습

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,7 +1,7 @@
 import MyComponent from "./MyComponent";
 
 const App = () => {
-  return <MyComponent name="laon">리액트</MyComponent>;
+  return <MyComponent name={"laon"} favoriteNumber={3}>리액트</MyComponent>;
 };
 
 export default App;
